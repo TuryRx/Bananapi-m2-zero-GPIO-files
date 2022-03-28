70c9585ae05908fdd37ad771621fe7ec22b04c5e
TuryRx <ar36@live.com.mx>
2022-03-27 21:20:30 -0600

    Update 1.4.4.1

--- m2z_button_state.py
+++ m2z_button_state.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+##Para utilizar boton sin resistencia
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+##Boton con resistencia de 10K a GND
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(31, GPIO.IN)
 
